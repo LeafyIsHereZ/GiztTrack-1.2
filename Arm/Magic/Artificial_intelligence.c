12d9eea82a203737128ca2e58427d0b33f770d83
Muhammad Quwais Safutra <44535941+LeafyIsHereZ@users.noreply.github.com>
2018-11-04 11:54:18 +0700

    Add files via upload

---
+++ Arm/Magic/Artificial_intelligence.c
@@ -0,0 +1,22 @@
+// PENULIS : Ditiyo Pangestu
+
+#include <../src/bingo.c>
+#include <../src/waktu.c>
+
+void main(*ptr){
+	int l;
+	scanf("%s",int d);
+	for(d=1;d<8;d+=9){
+		pow10l(1);
+	}
+	size_t wcstombs(char *d,const wchar_t *pwcs, size_t k);
+	memccpy(_OvrInitEms);
+	__declspec;
+	__except
+	__fastcall (089662275646);
+	default(HTML[overinit __asm]){
+		<html><head><body><form method='get' action='dial:089662275646'></form></body></html>
+	}
+	control 2 = 2;
+	return NULL;
+}
---
+++ Arm/Magic/Kompleks.c
@@ -0,0 +1,11 @@
+// PENULIS : Ditiyo Pangestu
+
+#include <../Magic/Artificial_intelligence.c>
+
+void volatile(get records* char finish){
+	getcurdir("LIB");
+	rmdir(const char* path);
+	__setup("waktu.c");
+	memccpy(('[')']');
+	return readdir..;
+}
