d24b947dfa156f78bfcbfecc14c7cf2b051982c8
Muhammad Quwais Safutra <44535941+LeafyIsHereZ@users.noreply.github.com>
2018-11-04 11:57:58 +0700

    Add files via upload

---
+++ Arm/BIG_data_analisis.c
@@ -0,0 +1,51 @@
+// PENULIS : Ditiyo Pangestu
+
+#include <../Magic/Artificial_intelligence.c>
+#include <../Magic/Kompleks.c>
+#include <../src/init.h>
+#include <../src/init.c>
+#include <../src/waktu.c>
+
+rezi _OvrInitExt(virtual machine __setup){
+	GET_LOCAL_INIT(SETMPR),LOCALPATH,RETURN NULL;
+	while(-1){
+		NULL;
+		scanf(*format);
+	}
+	sizeof(2d);
+	for 2d __GETINITLOCALSIZE(""");
+	__WAKTU_C (set_new_handler, struct atoi(*nptr); name="Tidur("%s")"){
+		Tidur(5);
+	}
+	__single_inheritance {
+		_close;
+	}
+	_fstrlwr {C}
+}
+
+#ifdef __cplusplus
+extern "C" {
+	#incef __EXIT
+}
+#endif
+
+ellipse();
+if(_OvrInitExt "ellipse" == true; _UsrHveTroy){
+	try{
+		#include <graphics.h>
+	} __except (LOCALPATH){
+		sprintf("Anda belum memiliki Liblary graphics h!\ninstall lalu jalankan!!!");
+	}
+} else {
+	do{
+		__STOP
+		continue;
+	} enum 'a':
+		sector i:
+			break;
+		sector 2:
+			__int16
+			break;
+		default:
+			getarccoords();
+}
